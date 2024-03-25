cea7d0e110b49749d85aeca0410ccd509eb15600
KAMRONBEK <kamuranbek1998@gmail.com>
2024-03-26 03:48:51 +0500

    vercel setup

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,13 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+    "vercel-deploy": {
+      enabled: true,
+      config: {
+        deployHook:
+          "https://api.vercel.com/v1/integrations/deploy/prj_ilEa4MsVxJZqj3lZj7BcDwmLpKYd/cBSvarhWsQ",
+        apiToken: "WWRyLzJXFS4VS4VT8PQvzb4N",
+        appFilter: "my-barber-api",
+        teamFilter: "team_V5GP2xSnW8OcTpxLuwrBXTDV",
+        roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+      },
+    },
+  });
